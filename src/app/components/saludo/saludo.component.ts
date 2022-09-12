0a198468f5059342779528ebf76b32a3df79c2b9
ivanretamozo <retamozoi445@gmail.com>
2022-09-12 17:45:41 -0300

    ciclo de vida de los componentes

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,16 +1,34 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.scss'],
 })
-export class SaludoComponent implements OnInit {
+export class SaludoComponent implements OnInit, OnDestroy, OnChanges {
   @Input() nombre: string = 'extraño';
   @Output() mensajeEmitter: EventEmitter<string> = new EventEmitter<string>();
   constructor() {}
   ngOnInit(): void {
     //instrucciones previas a la renderizacion del componente
+    console.log('hola desde el ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //instrucciones cuando el componente detecte cambios
+    console.log('se detectaron cambios');
+  }
+  ngOnDestroy(): void {
+    //instrucciones cuando el componente se destruya
+    console.log('el componente ah desaparecido');
   }
 
   /*
@@ -26,3 +44,16 @@ export class SaludoComponent implements OnInit {
     );
   }
 }
+
+/* 
+  ORDEN DE LOS CICLOS DE VIDA DE LOS COMPONENTES:
+  **1.NgOnChanges
+  **2.NgOnInit
+    3.NgAfterContentInit
+    4.NgAfterContentChecked
+    5.NgAfterViewInit
+    6.NgAfterViewChecked
+    7.NgAfterContentChecked
+    8.NgAfterViewChecked
+  **9.NgOnDestroy
+*/
